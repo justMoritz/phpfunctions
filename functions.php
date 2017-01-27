72b7d0bdc3daa1659803ac517eaed6ea45eab36f
justMoritz <moritzzimmer@moritzzimmer.com>
2017-01-27 13:56:48 -0600

    added doesContain function

--- functions.php
+++ functions.php
@@ -6,17 +6,28 @@
 // startsWith and endsWith Functions adapted from
 // http://phapalegaurav.blogspot.com/2013/10/php-startswith-and-endswith-functions.html
 // by Gaurav Phapale
+// Checks if a string ends with a substring
 function endsWith($haystack, $needle)
 {
     return $needle === "" || substr($haystack, -strlen($needle)) === $needle;
 } 
 
+// Checks if a string starts with a substring
 function startsWith($haystack, $needle)
 {
     return $needle === "" || strpos($haystack, $needle) === 0;
 }  
 
 
+// Function checks if a string contains a substring
+function doesContain($haystack, $needle){
+    if (strpos($haystack, $needle) !== false) {
+        return true;
+    }   
+    else{
+        return false;   
+    }
+}
 
 
 
